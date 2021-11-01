4213e37f8c9bef654c77a1670074dda18673413b
Luis Andres Rodriguez Campos <luis.andres.hamil@gmail.com>
2021-11-01 00:34:17 -0700

    servidor

--- GilioInventario/GilioInventario/settings.py
+++ GilioInventario/GilioInventario/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'USER': 'cocxzdrq',
         'PASSWORD': 'YPOd8Iwhcc-bxey69URCPap2OZX75hhE',
         'HOST': 'fanny.db.elephantsql.com',
-        'PORT_PORT': '5432',
+        'PORT_PORT': '5434',
     }
 }
 
