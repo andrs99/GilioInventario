694bca335a2c595c77172f18856a8a42af654603
Luis Andres Rodriguez Campos <luis.andres.hamil@gmail.com>
2021-11-19 02:29:05 -0600

    Acualizacion - Altas areas - Think

--- GilioInventario/settings.py
+++ GilioInventario/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '931h!84&$t&ofyz34rs+rit-%s15+p$2vg^$@ju5srfat6#250'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.72']
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
